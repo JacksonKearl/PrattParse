600896936483d98547903858ff7e225753c88580
Jackson Kearl <jakearl@microsoft.com>
2022-04-26 11:17:44 -0700

    nicest

--- src/calculator.demo.ts
+++ src/calculator.demo.ts
@@ -11,17 +11,6 @@ const { tokenize } = new Tokenizer([
 	{ pattern: ')' },
 	{ pattern: /\d+/, id: 'NUMBER' },
 ])
-	AddSub = 1,
-	MulDiv = 2,
-	Exp = 3,
-	Negate = 4,
-}
-
-export const calculate = new ParserBuilder<number, TokenMatchResult>(tokenize)
-	.registerPrefix('NUMBER', { parse: (_, token) => +token.value })
-	.registerPrefix('(', PARENTHESES_PARSELET)
-
-	.prefix('-', Precedence.Negate, (_, right) => -right)
 
 	.infixRight('^', Precedence.Exp, (left, _, right) => left ** right)
 
